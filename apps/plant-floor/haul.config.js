3758f4e30d1b930a894f3c49b12f420ac685704f
Stephen Woodruff <swoodruff@plex.com>
2021-04-01 14:24:55 -0400

    Revert back to work haul implementation

--- apps/plant-floor/haul.config.js
+++ apps/plant-floor/haul.config.js
@@ -1,10 +1,10 @@
-// import { withPolyfills, makeConfig } from "@haul-bundler/preset-0.60";
+import { withPolyfills, makeConfig } from "@haul-bundler/preset-0.60";
 
-// // eslint-disable-next-line import/no-default-export
-// export default makeConfig({
-//   bundles: {
-//     index: {
-//       entry: withPolyfills("./index")
-//     }
-//   }
-// });
+// eslint-disable-next-line import/no-default-export
+export default makeConfig({
+  bundles: {
+    index: {
+      entry: withPolyfills("./index"),
+    },
+  },
+});
