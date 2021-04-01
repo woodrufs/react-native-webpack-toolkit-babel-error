cb0e11e5af4f4b20fde0112a4b5a4f4c52c9c178
Stephen Woodruff <swoodruff@plex.com>
2021-04-01 14:21:18 -0400

    Remove reference to plex-eslint rules

--- tools/eslint-config/index.js
+++ tools/eslint-config/index.js
@@ -3,25 +3,24 @@ module.exports = {
   parser: "",
   ignorePatterns: ["*.d.ts"],
 
-  extends: ["plex-standards", "plex-standards/node", "plex-standards/react"],
+  extends: [],
 
   overrides: [
     {
       files: ["*.ts", "*.tsx"],
-      extends: "plex-standards/typescript",
       parser: "@typescript-eslint/parser",
       parserOptions: {
         project: "./tsconfig.json",
         ecmaVersion: 2018,
-        sourceType: "module"
+        sourceType: "module",
       },
       rules: {
         "@typescript-eslint/explicit-member-accessibility": "off",
         "react/jsx-handler-names": "off",
         "import/no-unresolved": "off",
         "import/extensions": "off",
-        "import/no-default-export": "off"
-      }
-    }
-  ]
+        "import/no-default-export": "off",
+      },
+    },
+  ],
 };
